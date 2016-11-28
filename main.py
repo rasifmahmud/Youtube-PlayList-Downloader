d8f234f43db74b5194296993c7bc5e8403d856dc
rasifmahmud <rasifmahmud16@gmail.com>
2016-11-28 14:06:51 +0600

    main file upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+from pytube import YouTube
+url = 'https://www.youtube.com/watch?v=givuYd_cKm0&list=PL6gx4Cwl9DGD25IGk9Xf7oC3wiT9gC75x'
+source_code = requests.get(url, allow_redirects=False)
+plain_text = source_code.text.encode('ascii', 'replace')
+soup = BeautifulSoup(plain_text, 'html.parser')
+list = []
+for link in soup.findAll('li', {'class': 'yt-uix-scroller-scroll-unit '}):
+    id = link.get('data-video-id')
+    list.append(id)
+
+
+
+counter=49
+print(list[198])
+print(len(list))
+
+#index number 48 is missing to download
+#another one missing may be 9 or 10 or 11
+for i in range(49,len(list)):
+    id = 'https://www.youtube.com/watch?v='+list[i]
+    yt = YouTube(id)
+    video = yt.get('mp4', '720p')
+    video.download('photoshop'+str(counter))
+    print(counter)
+    counter=counter+1
+
+
